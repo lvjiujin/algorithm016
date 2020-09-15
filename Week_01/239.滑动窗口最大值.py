360b93060248d2a3a1576a1fd96b4d0064b9bf82
lvjiujin <lvjiujin@gmail.com>
2020-09-15 10:51:49 +0800

    clean version code about maxSlidingWindows and add time and space complexity analysis

--- Week_01/239.滑动窗口最大值.py
+++ Week_01/239.滑动窗口最大值.py
@@ -8,7 +8,7 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         from collections import deque
-        #  单调栈的思想, monotonic stack.
+        #  单调栈的思想, monotonic stack. 时间复杂度O(n), 空间复杂度O(n) 
         if not nums:
             return []
         if len(nums) == 1 or k == 1:
@@ -30,6 +30,29 @@ class Solution:
 
         result = []
         # 循环的次数就是滑动窗口的个数（除了第一个，因为第一个在上面的循环中，当然两个循环可以合二为一，加一个判断即可)
+        """
+        q = deque()
+        result = []
+        
+        for i in range(len(nums)):
+            # the first/left (max) element is out of the current window
+            if q and i - q[0] == k:
+                q.popleft()
+            
+            while q:
+                # pop useles elements from last/right of the queue
+                if nums[q[-1]] < nums[i]:
+                    q.pop()
+                else:
+                    break
+            
+            q.append(i)
+            
+            if i >= k-1: # i == k-1 is the beginning of a full window
+                result.append(nums[q[0]])
+            
+        return result
+        """
         # 最后一个滑动窗口的最大值，在这个循环中没有加入到result中，所以最后要单独result.append(nums[deq[0]])
         for i in range(k, len(nums)):
             result.append(nums[deq[0]])
@@ -46,6 +69,28 @@ class Solution:
         result.append(nums[deq[0]])
         return result
         
+        # 同样思想的另一种写法,更简洁
+        '''
+        Keep indexes of good candidates in deque d. The indexes in d are from the current window, they're increasing, and their corresponding nums are decreasing. Then the first deque element is the index of the largest window value.
+
+For each index i:
+
+1. Pop (from the end) indexes of smaller elements (they'll be useless).
+2. Append the current index.
+3. Pop (from the front) the index i - k, if it's still in the deque (it falls out of the window).
+4. If our window has reached size k, append the current window maximum to the output.
+        '''
+        d = deque()
+        out = []
+        for i, n in enumerate(nums):
+            while d and nums[d[-1]] < n:
+                d.pop()
+            d += i,
+            if d[0] == i - k:
+                d.popleft()
+            if i >= k - 1:
+                out += nums[d[0]],
+        return out
     
 
 
