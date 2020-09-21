79d74ef5f05136054298e41a500d9846e85d5f76
lvjiujin <lvjiujin@gmail.com>
2020-09-21 18:03:29 +0800

    review max heap and heap algorithm

--- Week_02/239.滑动窗口最大值.py
+++ Week_02/239.滑动窗口最大值.py
@@ -14,15 +14,29 @@ class Solution:
 		# Time complexity : O(NlogN)
 		# Space complexity: O(N)
         # heap method, but 效率非常低，了解有这种方法就行。
-        import heapq
-        res, heap = [], []
-        for i in range(len(nums)):
-            heapq.heappush(heap, (-nums[i], i))
-            if i + 1 >= k:
-                while heap and heap[0][1] < i +1 - k:
-                    heapq.heappop(heap)
-                res.append(-heap[0][0])
-        return res
+        # import heapq
+        # res, heap = [], []
+        # for i in range(len(nums)):
+        #     heapq.heappush(heap, (-nums[i], i))
+        #     if i + 1 >= k:
+        #         while heap and heap[0][1] < i +1 - k:
+        #             heapq.heappop(heap)
+        #         res.append(-heap[0][0])
+        # return res
+        
+        # 2nd methodmonotonic decrease stack 
+        from collections import deque
+        d, out = deque(), []
+        for i, e in enumerate(nums):
+            while d and e > nums[d[-1]]:
+                d.pop()
+            d.append(i)
+            if d[0] == i - k:
+                d.popleft()
+            if i >= k-1:
+                out.append(nums[d[0]])
+        
+        return out
 
 
 # @lc code=end
