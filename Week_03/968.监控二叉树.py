b3f78b39e2f0fa71387690852efef4bc102224eb
lvjiujin <lvjiujin@gmail.com>
2020-09-25 11:03:46 +0800

    dp + recursion, when look at it,it is easy to understand, but hard to analysis

--- Week_03/968.监控二叉树.py
+++ Week_03/968.监控二叉树.py
@@ -14,6 +14,23 @@
 
 class Solution:
     def minCameraCover(self, root: TreeNode) -> int:
+        # 感觉本题目看着简单，分析起来好费劲呀。
+        # 给出官方链接，慢慢品吧。递归+动态规划
+        # https://leetcode-cn.com/problems/binary-tree-cameras/solution/jian-kong-er-cha-shu-by-leetcode-solution/
+        def dfs(root: TreeNode) -> List[int]:
+            if not root:
+                return [float("inf"), 0, 0]
+            
+            la, lb, lc = dfs(root.left)
+            ra, rb, rc = dfs(root.right)
+            a = lc + rc + 1
+            b = min(a, la + rb, ra + lb)
+            c = min(a, lb + rb)
+            return [a, b, c]
+        
+        a, b, c = dfs(root)
+        return b
+
         
 # @lc code=end
 
