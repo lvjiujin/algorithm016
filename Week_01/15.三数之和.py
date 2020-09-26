86dd7974abe9e2b892c31bc30f2d2ac36d6973a4
lvjiujin <lvjiujin@gmail.com>
2020-09-26 12:27:07 +0800

    optimize the 3sum if nums[i] > 0 break

--- Week_01/15.三数之和.py
+++ Week_01/15.三数之和.py
@@ -9,48 +9,35 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         if not nums or len(nums) <3:
             return []
-        nums.sort()
+        
         result, N = [], len(nums)
-
+        nums.sort()
+        
         for i in range(N-1):
+            if nums[i] > 0:
+                break
             target = - nums[i]
             # convert 3sum to 2sum problem
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            s, e = i + 1, N-1
-            while s < e:
-                if nums[s] + nums[e] == target:
-                    result.append([nums[i], nums[s], nums[e]])
-                    s += 1
-                    e -= 1
-                    while s < e and nums[s] == nums[s-1]:
-                        s += 1
-                    while s < e and nums[e] == nums[e+1]:
-                        e -= 1
-                elif nums[s] + nums[e] < target:
-                    s += 1
-                elif nums[s] + nums[e] > target:
-                    e -= 1
+            
+            l, r = i + 1, N-1
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    result.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    r -= 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
         
         return result
-        # 不是很明白，为何下面的做法比上面的快很多。
-        # nums.sort()
-        # N, result = len(nums), []
-        # for i in range(N):
-        #     if i > 0 and nums[i] == nums[i-1]:
-        #         continue
-        #     target = nums[i]*-1
-        #     s,e = i+1, N-1
-        #     while s<e:
-        #         if nums[s]+nums[e] == target:
-        #             result.append([nums[i], nums[s], nums[e]])
-        #             s = s+1
-        #             while s<e and nums[s] == nums[s-1]:
-        #                 s = s+1
-        #         elif nums[s] + nums[e] < target:
-        #             s = s+1
-        #         else:
-        #             e = e-1
-        # return result
+        
 # @lc code=end
 
