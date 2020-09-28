8f9a06da3df405fa0ab56e1fd6845eb37bd51ee6
lvjiujin <lvjiujin@gmail.com>
2020-09-28 12:04:35 +0800

    non-recursion for reverse the binary tree

--- Week_03/226.翻转二叉树.py
+++ Week_03/226.翻转二叉树.py
@@ -16,12 +16,22 @@ class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         # 思路：从根节点开始，递归地对树进行遍历，并从叶子结点先开始翻转。
         # 如果当前遍历到的节点 root 的左右两棵子树都已经翻转，那么我们只需要交换两棵子树的位置，即可完成以root 为根节点的整棵子树的翻转
-
-        if not root:
-            return root
-        left = self.invertTree(root.left)
-        right = self.invertTree(root.right)
-        root.left, root.right = right, left
+        # recursion
+        # if not root:
+        #     return root
+        # left = self.invertTree(root.left)
+        # right = self.invertTree(root.right)
+        # root.left, root.right = right, left
+        # return root
+        # non-recursion
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                stack.append(node.left)
+                stack.append(node.right)
+                node.left, node.right = node.right, node.left
         return root
+
 # @lc code=end
 
