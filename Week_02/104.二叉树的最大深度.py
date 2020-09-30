f655cc72ed51071509bb5608f47902bd24a2c1d4
lvjiujin <lvjiujin@gmail.com>
2020-09-30 23:12:20 +0800

    modified the bfs for max depth in binary tree

--- Week_02/104.二叉树的最大深度.py
+++ Week_02/104.二叉树的最大深度.py
@@ -14,29 +14,30 @@
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        # 方法一：递归法
+        # 方法一：递归法 （比较快）
         # if not root:
         #     return 0
         # else:
         #     left_max = self.maxDepth(root.left)
         #     right_max = self.maxDepth(root.right)
         #     return max(left_max, right_max) + 1
-        # 方法二：广度优先 BFS
+        # 方法二：广度优先 BFS （层序遍历，相当于套用模板) 比较慢
+        # if not root:
+        #     return 0
         # depth = 0
-        # level = [root] if root else []
-        # while level:
+        # queue = [root]
+        # while queue:
+        #     size  = len(queue)
         #     depth += 1
-        #     queue = []
-        #     for el in level:
-        #         if el.left:
-        #             queue.append(el.left)
-        #         if el.right:
-        #             queue.append(el.right)
+        #     for _ in range(size):
+        #         node = queue.pop(0)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
             
-        #     level = queue
-
         # return depth
-        # 方法三：广度优先 DFS
+        # 方法三：深度优先 DFS 迭代法
         depth = 0
         stack = [(root,1)]
         
@@ -47,7 +48,8 @@ class Solution:
             
             if leng > depth:
                 depth = leng            
-            
+            # 注意：这里一定要是先右后左的顺序入栈，这样出栈的顺序才符合先左子树，后右子树。
+            # 每深入一层root.right 或root.left, length就加一。
             if root.right:
                 stack.append((root.right,leng+1))
             if root.left:
