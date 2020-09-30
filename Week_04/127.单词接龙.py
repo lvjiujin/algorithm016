066a8b0a251d755141730afba9769ea82f054d4c
lvjiujin <lvjiujin@gmail.com>
2020-09-30 17:57:54 +0800

    BFS and defaultdict in word ladders

---
+++ Week_04/127.单词接龙.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode.cn id=127 lang=python3
+#
+# [127] 单词接龙
+#
+
+# @lc code=start
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # another two approaches see: https://leetcode-cn.com/problems/word-ladder/solution/yan-du-you-xian-bian-li-shuang-xiang-yan-du-you-2/
+        from collections import defaultdict
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+
+        # Since all words are of same length.
+        L = len(beginWord)
+
+        # Dictionary to hold combination of words that can be formed,
+        # from any given word. By changing one letter at a time.
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+        # print("all_combo_dict = ", all_combo_dict)
+
+        # Queue for BFS
+        queue = [(beginWord, 1)]
+        # Visited to make sure we don't repeat processing same word.
+        visited = {beginWord: True}
+        while queue:
+            current_word, level = queue.pop(0)
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+                # Next states are all the words which share the same intermediate state.
+                # when the key (here is the variable 'intermediate_word' didn't exist not int the defaultdict 
+                # (here is the 'all_combo_dict' the defaultdict class can automatically return a null list with default_factory
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e. the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited[word] = True
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
+        return 0
+    
+# @lc code=end
+
